d2d0a55e8dbab53f1b6ff3578e5ad0ed234bab7a
adamwise <awise.developer@gmail.com>
2023-10-07 20:46:20 -0700

    Added crud functionality to a new service layer for a jokes api

---
+++ src/test/java/com/example/jokesapi/JokesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jokesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
